07c0155ded6e0bedcb59856ce2d597f9bd36559a
mrrs878 <mrrs878@foxmail.com>
2021-08-18 21:09:30 +0800

    fix: 修改hooks/index.ts导出方式

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,16 +1,16 @@
 /*
  * @Author: mrrs878
  * @Date: 2020-12-10 11:21:02
- * @LastEditTime: 2021-08-18 17:49:07
+ * @LastEditTime: 2021-08-18 21:09:00
  * @LastEditors: mrrs878@foxmail.com
  * @Description: In User Settings Edit
- * @FilePath: \wrench\packages\hooks\src\index.ts
+ * @FilePath: \gear\packages\hooks\src\index.ts
  */
 import useDocumentTitle from './useDocumentTitle';
 import useInputValue from './useInputValue';
 import useRequest from './useRequest';
 import useWindowScroll from './useWindowScroll';
 
-export default {
+export {
   useDocumentTitle, useInputValue, useRequest, useWindowScroll,
 };
