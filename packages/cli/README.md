2e956cce1b6a0b3168ce98e546616dec27340aad
mrrs878 <mrrs878@foxmail.com>
2021-09-06 20:29:04 +0800

    feat: 添加@mrrs878/cli

---
+++ packages/cli/README.md
@@ -0,0 +1,11 @@
+# `@mrrs878/cli`
+
+> TODO: description
+
+## Usage
+
+```
+const cli = require('@mrrs878/cli');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/cli/__tests__/cli.test.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: mrrs878@foxmail.com
+ * @Date: 2021-09-06 19:39:25
+ * @LastEditors: mrrs878@foxmail.com
+ * @LastEditTime: 2021-09-06 20:28:17
+ * @FilePath: \gear\packages\cli\__tests__\cli.test.ts
+ */
+describe('@mrrs878/cli', () => {
+  it('needs tests', () => undefined);
+});
