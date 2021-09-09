a60da717e7ad47ae39a42cdd716a2ca2688651e8
mrrs878 <mrrs878@foxmail.com>
2021-09-09 19:22:28 +0800

    perf: 优化rollup打包配置

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
  * @Author: mrrs878@foxmail.com
  * @Date: 2021-08-18 13:58:53
  * @LastEditors: mrrs878@foxmail.com
- * @LastEditTime: 2021-08-18 16:21:59
- * @FilePath: \bytemd\README.md
+ * @LastEditTime: 2021-09-09 19:20:57
+ * @FilePath: \gear\README.md
 -->
 [![Node.js CI](https://github.com/mrrs878/gear/actions/workflows/main.yml/badge.svg)](https://github.com/mrrs878/gear/actions/workflows/main.yml)
 [![Coverage Status](https://coveralls.io/repos/github/mrrs878/gear/badge.svg?branch=main)](https://coveralls.io/github/mrrs878/gear?branch=main)
 
-packages/hooks/README.md
+![npm](https://img.shields.io/npm/v/@mrrs878/cli?label=%40mrrs878%2Fcli)
+![npm](https://img.shields.io/npm/v/@mrrs878/hooks?label=%40mrrs878%2Fhooks)
+![npm](https://img.shields.io/npm/v/@mrrs878/save-all-resources?label=%40mrrs878%2Fsave-all-resources)
--- rollup.config.js
+++ rollup.config.js
@@ -9,9 +9,9 @@ import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import replace from '@rollup/plugin-replace';
 import postcss from 'rollup-plugin-postcss';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 const production = !process.env.ROLLUP_WATCH;
-const umd = process.env.UMD;
 
 const packages = fs.readdirSync(path.resolve(__dirname, 'packages'));
 
@@ -76,6 +76,7 @@ const configs = packages
         'stream',
         'constants',
         'events',
+        'ora',
         ...Object.keys(pkg.dependencies || {}),
         ...Object.keys(pkg.peerDependencies || {}),
       ],
@@ -88,23 +89,6 @@ const configs = packages
       sourcemap: true,
       globals: { react: 'react' },
       inlineDynamicImports: true,
-    };
-
-    /** @type {import('rollup').RollupOptions} */
-    const umdConfig = {
-      ...common,
-      output: [
-        {
-          ...umdOutputOption,
-          file: path.resolve('packages', key, 'dist/index.js'),
-        },
-        {
-          ...umdOutputOption,
-          file: path.resolve('packages', key, 'dist/index.min.js'),
-          plugins: [terser()],
-        },
-      ],
-      external: Object.keys(pkg.peerDependencies || {}),
     };
 
     /** @type {import('rollup').RollupOptions} */
@@ -128,13 +112,11 @@ const configs = packages
           babelHelpers: 'runtime',
           extensions: ['.js', '.mjs', '.html'],
         }),
+        nodePolyfills(),
       ],
     };
 
-    if (umd) {
-      return [config, umdConfig, es5Config];
-    }
-    return [config];
+    return [config, es5Config];
   })
   .flat();
 
